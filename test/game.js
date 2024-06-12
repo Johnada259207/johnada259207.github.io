4abe971f2d8db1ba4151d18b61c3c494e1ed4181
Johnada259207 <johnadamsperez@gmail.com>
2024-06-11 21:38:50 -0700

    Update game.js

--- test/game.js
+++ test/game.js
@@ -13,26 +13,61 @@ const player = {
     dy: 0,
     gravity: 0.5,
     jumpPower: -10,
-    onGround: false
+    onGround: false,
+    sprite: new Image()
 };
 
-const platforms = [
-    { x: 0, y: 580, width: 800, height: 20 },
-    { x: 100, y: 450, width: 200, height: 20 },
-    { x: 400, y: 350, width: 200, height: 20 },
-    { x: 650, y: 250, width: 100, height: 20 }
+player.sprite.src = 'assets/player.png';
+
+const platformSprite = new Image();
+platformSprite.src = 'assets/platform.png';
+
+const doorSprite = new Image();
+doorSprite.src = 'assets/door.png';
+
+const background = new Image();
+background.src = 'assets/background.png';
+
+const levels = [
+    {
+        platforms: [
+            { x: 0, y: 580, width: 800, height: 20 },
+            { x: 100, y: 450, width: 200, height: 20 },
+            { x: 400, y: 350, width: 200, height: 20 },
+            { x: 650, y: 250, width: 100, height: 20 }
+        ],
+        door: { x: 750, y: 210, width: 40, height: 40 }
+    },
+    {
+        platforms: [
+            { x: 0, y: 580, width: 800, height: 20 },
+            { x: 150, y: 450, width: 200, height: 20 },
+            { x: 450, y: 350, width: 200, height: 20 },
+            { x: 700, y: 250, width: 100, height: 20 }
+        ],
+        door: { x: 50, y: 210, width: 40, height: 40 }
+    }
 ];
 
+let currentLevel = 0;
+
 function drawPlayer() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(player.x, player.y, player.width, player.height);
+    ctx.drawImage(player.sprite, player.x, player.y, player.width, player.height);
 }
 
 function drawPlatforms() {
-    ctx.fillStyle = 'green';
-    platforms.forEach(platform => {
-        ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+    levels[currentLevel].platforms.forEach(platform => {
+        ctx.drawImage(platformSprite, platform.x, platform.y, platform.width, platform.height);
     });
+}
+
+function drawDoor() {
+    const door = levels[currentLevel].door;
+    ctx.drawImage(doorSprite, door.x, door.y, door.width, door.height);
+}
+
+function drawBackground() {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 }
 
 function clear() {
@@ -45,11 +80,12 @@ function newPos() {
     player.y += player.dy;
 
     detectCollisions();
+    detectDoor();
 }
 
 function detectCollisions() {
     player.onGround = false;
-    platforms.forEach(platform => {
+    levels[currentLevel].platforms.forEach(platform => {
         if (
             player.x < platform.x + platform.width &&
             player.x + player.width > platform.x &&
@@ -62,7 +98,6 @@ function detectCollisions() {
         }
     });
 
-    // Prevent player from going out of canvas
     if (player.x < 0) player.x = 0;
     if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
     if (player.y + player.height > canvas.height) {
@@ -72,9 +107,25 @@ function detectCollisions() {
     }
 }
 
+function detectDoor() {
+    const door = levels[currentLevel].door;
+    if (
+        player.x < door.x + door.width &&
+        player.x + player.width > door.x &&
+        player.y < door.y + door.height &&
+        player.y + player.height > door.y
+    ) {
+        currentLevel = (currentLevel + 1) % levels.length;
+        player.x = 50;
+        player.y = 550;
+    }
+}
+
 function update() {
     clear();
+    drawBackground();
     drawPlatforms();
+    drawDoor();
     drawPlayer();
 
     newPos();
@@ -127,7 +178,8 @@ function saveGame() {
         player: {
             x: player.x,
             y: player.y
-        }
+        },
+        currentLevel
     };
     localStorage.setItem('gameState', JSON.stringify(gameState));
 }
@@ -137,6 +189,7 @@ function loadGame() {
     if (gameState) {
         player.x = gameState.player.x;
         player.y = gameState.player.y;
+        currentLevel = gameState.currentLevel;
     }
 }
 
